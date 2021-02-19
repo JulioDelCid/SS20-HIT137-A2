e651ca66503d83769f74e2f500a1caf15a5330ab
JulioDelCid <40550521+JulioDelCid@users.noreply.github.com>
2021-02-19 11:28:01 +1100

    Add files via upload

---
+++ A2-Q1-TurtleDraw.py
@@ -0,0 +1,45 @@
+# Assigment 2:      Question 1 - Turtle draw
+# Contributors:     Julio Del Cid studentid 332808
+# GITHUB Repo:      https://github.com/JulioDelCid/SS20-HIT137-A2/ 
+#
+#
+#############       Commence assignmet code ##############################
+
+
+import turtle
+from turtle  import Screen, Turtle
+
+# Set up the environment and turtle
+screen = Screen()
+t = Turtle("turtle")
+t.speed(-1)
+t.pencolor('red')
+t.color("red", "green")
+t.pensize(5)
+
+# Screen background colour and 
+screen.bgcolor("grey")
+
+# These parameters will be the mouse position
+def mousedrag(x, y):  
+    t.ondrag(None)
+    t.setheading(t.towards(x, y))
+    t.goto(x, y)
+    t.ondrag(mousedrag)
+
+
+def clickRight():
+    t.clear()
+    
+# This will run the program
+def main():  
+    turtle.listen()
+
+    
+# When we drag the turtle object call dragging
+    t.ondrag(mousedrag) 
+    turtle.onscreenclick(clickRight, 3)
+
+# This prevents closing the program by continuing to run main() 
+    screen.mainloop()  
+main()
